f69b66e510122070b1396329f7930bfa11e2b1fb
ArmenianDolan <159221228+ArmenianDolan@users.noreply.github.com>
2024-02-07 06:04:52 +0100

    Add files via upload

---
+++ js/gift/code.js
@@ -0,0 +1,77 @@
+
+
+
+//________________________________________________
+function copyEvent(id)
+{
+     alert("REP-EU-5000-UTCYKM6MEW3CA5HS");
+  var str = document.getElementById(id);
+  window.getSelection().selectAllChildren(str);
+  document.execCommand("CODE")
+}
+
+
+function copyEvent2(id)
+{
+     alert("0471554668643488");
+  var str = document.getElementById(id);
+  window.getSelection().selectAllChildren(str);
+  document.execCommand("CODE")
+}
+
+
+function copyEvent3(id)
+{
+     alert("0927976203330558");
+  var str = document.getElementById(id);
+  window.getSelection().selectAllChildren(str);
+  document.execCommand("CODE")
+}
+
+
+//________________________________________________
+
+/*
+var a;
+function show_hide()
+{
+if(a==1)
+{
+document.getElementById("img2").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("img2").style.display="none";
+return a=1;
+}
+}
+
+//_____________________________________________
+var showimage = document.getElementById(button);
+
+  showimage.addEventListener("click", function(showimage){
+  showimage.target.calssList.toggle("show image");
+  })
+
+
+
+function clicked(){
+
+
+if(img==1){
+  document.getElementById("img").style.display="inline"
+  return img= 0;
+}else{
+  document.getAnimations("img").style.display="none"
+  return img= 1;
+}
+
+}
+
+
+*/
+
+
+
+//________________________________________________
---
+++ js/us/us.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        });
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    setInterval(nextSlide, 5555); // Change slide every 5 seconds
+    showSlide(currentIndex); // Show initial slide
+});
